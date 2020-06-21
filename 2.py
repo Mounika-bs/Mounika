49034678ee41caeaa5ef29e1e9bfebb56a06b414
Mounika-bs <67216346+Mounika-bs@users.noreply.github.com>
2020-06-21 17:13:22 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,7 @@
+print('The principal is', p) 
+    print('The time period is', t) 
+    print('The rate of interest is',r) 
+      
+    si = (p * t * r)/100
+      
+    print('The Simple Interest is', si)

eb0b038819414f89bd41baa5bf76faa6672e8df0
Mounika-bs <67216346+Mounika-bs@users.noreply.github.com>
2020-06-21 17:21:49 +0530

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,7 @@
+PI = 3.14
+radius = float(input(' Please Enter the radius of a circle: '))
+
+diameter = 2 * radius
+circumference = 2 * PI * radius
+print('diameter of the circle is:',diameter)
+print('circumference of the circle is:',circumference)

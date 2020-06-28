4b418401ddae04b4ee3d6833bb27a974f0afa86b
Mounika-bs <67216346+Mounika-bs@users.noreply.github.com>
2020-06-28 17:01:50 +0530

    Add files via upload

---
+++ (m3,ch-1)4.py
@@ -0,0 +1,11 @@
+#to print the pattern
+n=5;
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+for i in range(n,5):
+    for j in range(i):
+        print('* ', end="")
+    print('')
